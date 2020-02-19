6a3e9deeee2a788f49cc9894cbbdc067f27a3e40
george sadej <gsadej@comcast.net>
2020-02-19 21:05:39 +0000

    Added currency conversion feature

--- src/main/java/org/springframework/nanotrader/SpringtraderMarketSummaryApplication.java
+++ src/main/java/org/springframework/nanotrader/SpringtraderMarketSummaryApplication.java
@@ -1,51 +1,32 @@
-/*
- * Copyright 2002-2012 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.springframework.nanotrader.web.controller;
-
-import java.util.Locale;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.nanotrader.data.domain.MarketSummary;
-import org.springframework.nanotrader.data.util.CurrencyUtils;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-/**
- * Provides JSON based REST api to Market Summary
- * 
- * @author Brian Dussault
- */
-@Controller
-public class MarketSummaryController extends BaseController {
-
-  @RequestMapping(value = "/marketSummary", method = RequestMethod.GET)
-  public ResponseEntity<MarketSummary> findMarketSummary(Locale locale) {
-    return new ResponseEntity<MarketSummary>(
-        CurrencyUtils.convertCurrency(getTradingServiceFacade().findMarketSummary(), locale),
-        getNoCacheHeaders(),
-        HttpStatus.OK
-    );
-  }
-
-  @RequestMapping(value = "/marketSummary", method = RequestMethod.POST)
-  @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-  public void post() {
-  }
-
+package org.springframework.nanotrader;import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import io.opentracing.Tracer;import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;@SpringBootApplication
+@ComponentScan
+public class SpringtraderMarketSummaryApplication {	public static void main(String[] args) {
+		SpringApplication.run(SpringtraderMarketSummaryApplication.class, args);
+	}	@Bean
+	public WebMvcConfigurer webMvcConfigurerConfigurer(HandlerInterceptor interceptor) {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(interceptor);
+			}
+		};
+	}	@Bean
+	public HandlerInterceptor tracingInterceptor(Tracer tracer) {
+		return new HandlerInterceptor() {
+			@Override
+			public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+				if(tracer.activeSpan() != null) {
+					tracer.activeSpan().setTag("http.locale", String.valueOf(request.getLocale()));
+				}
+				return true;
+			}
+		};
+	}
 }
